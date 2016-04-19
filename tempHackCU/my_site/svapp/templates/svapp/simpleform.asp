235fad06d6de4fddc690671fbcbce9f0d04d2bc8
kushncode <aajaramillo95@gmail.com>
2016-04-19 13:16:27 -0600

    cleared old project files

--- tempHackCU/my_site/svapp/templates/svapp/simpleform.asp
+++
@@ -1,6 +0,0 @@
-<body>
-Rendering Visualization for 
-<%
-response.write(request.querystring("url")
-%>
-</body>
--- tempHackCU/my_site/svapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^svapp/simpleform', views.extract, name='extract'),
-    url(r'^$', views.index, name='index')]
-
